54b61e72d00f3690ecbc39b1540b7e7e26cc8eb5
darrenzhang <darrenzhang@91app.com>
2017-10-05 15:15:01 +0800

    init

--- Codewards_Who likes it/UnitTest1.cs
+++ Codewards_Who likes it/UnitTest1.cs
@@ -6,8 +6,24 @@ namespace Codewards_Who_likes_it
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_Null_Should_Be_noOneLikeThis()
         {
+            string[] input = null;
+
+            var actual = Kata.Likes(input);
+
+            var expected = "no one likes this";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+    public class Kata
+    {
+        public static string Likes(string[] input)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
