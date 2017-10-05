79a3799a004ba983f35dac6abbcc1034f59a7e43
darrenzhang <darrenzhang@91app.com>
2017-10-05 14:45:57 +0800

    加入專案檔案。

---
+++ Codewards_Who likes it/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewards_Who_likes_it
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
